028bbfff46e3d06bff739f36165d2a0ee575edb8
56fgfg <43039702+56fgfg@users.noreply.github.com>
2019-01-14 17:05:01 -0500

    CSD_2214_6 class hand in work

---
+++ Script1.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+// name - Harinder Kaur Grewal
+// Stident-id = C0742778
+//Course code= CSD_2214_6
+alert("hello")
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[namr="endDate"]');
+const clock = document.querySelector('clock');
+const daySpan = clock.querySelector('days');
+const hoursSpan = clock.querySelector('hours');
+const minutesSpan = clock.querySelector('minutes');
+const secondsSpan = clock.querySelector('seconds');
